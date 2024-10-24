981eb96869d16141a9a4ec8573192a0d311c001c
Christean Ghobrial <christeang01@outlook.de>
2024-10-24 19:48:09 +0200

    updated projects component
    now containing basic UI and fetch logic

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CardModule} from "primeng/card";
+import {PanelModule} from "primeng/panel";
+import {Button} from "primeng/button";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    PanelModule,
+    Button
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+  protected projects: any[] = [];
 
+  async ngOnInit() {
+    const response = await fetch("https://api.github.com/users/Chris-GM/repos");
+    const json = await response.json();
+    this.projects = (<any[]>json).map(element => {
+      console.log(element)
+
+      return {
+        name: element.name,
+        url: element.html_url,
+        description: element.description
+      }
+    });
+
+    console.log(this.projects)
+  }
+
+  openUrl(url: string) {
+    open(url);
+  }
 }
