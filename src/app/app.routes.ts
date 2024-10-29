ac8ffacddca9eb81230ce6485c90c9206ed588a2
Christean Ghobrial <christeang01@outlook.de>
2024-10-29 21:48:45 +0100

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import {ProjectsComponent} from "./components/projects/projects.component";
 import {LegalComponent} from "./components/legal/legal.component";
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/about', pathMatch: 'full'},
   {path: 'about', component: AboutComponent, title: 'About me'},
   {path: 'projects', component: ProjectsComponent, title: 'Projects'},
   {path: 'legal', component: LegalComponent, title: 'Legal Notice'},
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
   {path: '**', redirectTo: '/about'}
 ];
